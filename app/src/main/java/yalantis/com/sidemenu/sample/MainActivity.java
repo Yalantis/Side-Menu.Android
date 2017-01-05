a70cae42fc4bde8b1e823cecce125ab1727c32c7
dima.sholom <dima.sholom@yalantis.com>
2017-01-05 15:51:46 +0200

    Updated versions

--- app/src/main/java/yalantis/com/sidemenu/sample/MainActivity.java
+++ app/src/main/java/yalantis/com/sidemenu/sample/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
